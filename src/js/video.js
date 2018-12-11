7b67264eb4785f019930e38769e13ccb8b3cfb52
kevinmalats <ken_1721@hotmail.es>
2018-12-11 12:39:32 -0500

    d

--- src/js/video.js
+++
@@ -1,7 +0,0 @@
-var btnVideo= document.getElementById("recargarvideo");
-btnVideo.addEventListener("click", recargarvideo);
-
-function recargarvideo(){
-  let video= document.getElementById("videExplicacion");
-  video.play();
-}
